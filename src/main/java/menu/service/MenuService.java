e78e74730b7a4bcbeed57ef9a678a53dc8f9183e
ParkJiYeoung8297 <wldud8297@gmail.com>
2025-12-21 17:26:32 +0900

    refactor: Service에서 외부 라이브러리인 Random 분리

--- src/main/java/menu/service/MenuService.java
+++ src/main/java/menu/service/MenuService.java
@@ -3,6 +3,7 @@ package menu.service;
 import camp.nextstep.edu.missionutils.Randoms;
 import menu.domain.Menu;
 import menu.exception.Validator;
+import menu.util.RandomGenerator;
 
 import java.util.*;
 
@@ -13,9 +14,10 @@ public class MenuService {
         Set recommendedCategories = new HashSet();
         // 월 ~ 금까지 메뉴 추천
         while (recommendedCategories.size() < 5) {
-            int num = Randoms.pickNumberInRange(1, 5);
+            int num = RandomGenerator.getRandomNumber();
             Validator.validateRange(num, 1, 5);
             String category = Menu.getCategoriesByNumber(num);
+
             if (recommendedCategories.contains(category)) {
                 continue;
             }
@@ -30,7 +32,7 @@ public class MenuService {
                 }
 
                 while (menu.equals("INVALID")) {
-                    menu = Randoms.shuffle(menus).get(0);
+                    menu = RandomGenerator.getRandomMenu(menus);
                     // 해당 코치가 싫어하는 음식인지 확인 + 이미 먹은 음식인지 확인
                     if (hateMenu.get(name).contains(menu) || finalRecommenedMenu.get(name).contains(menu)) {
                         menu = "INVALID";
