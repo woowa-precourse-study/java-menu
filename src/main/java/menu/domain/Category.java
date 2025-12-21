c36442cf9d3f64ba12eceb25c6c16580af89fcbd
gudwns1812 <gudwns1812@naver.com>
2025-12-21 15:43:07 +0900

    feat: 초기 메뉴 세팅

--- src/main/java/menu/domain/Category.java
+++ src/main/java/menu/domain/Category.java
@@ -1,4 +1,25 @@
 package menu.domain;
 
 public enum Category {
+    JAPANESE("일식", 1),
+    KOREAN("한식", 2),
+    CHINESE("중식", 3),
+    ASIAN("아시안", 4),
+    WESTERN("양식", 5);
+
+    private final String name;
+    private final int value;
+
+    Category(String name, int value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
--- src/main/java/menu/factory/ApplicationFactory.java
+++ src/main/java/menu/factory/ApplicationFactory.java
@@ -1,9 +1,13 @@
 package menu.factory;
 
+import java.util.List;
+import java.util.Map;
 import menu.controller.InputParser;
 import menu.controller.MenuController;
+import menu.domain.Category;
 import menu.domain.MenuBoard;
 import menu.domain.MenuRecommender;
+import menu.domain.Menus;
 
 public class ApplicationFactory {
 
@@ -20,6 +24,34 @@ public class ApplicationFactory {
     }
 
     private MenuBoard initMenuBoard() {
-        return null;
+        Map<Category, Menus> initMenus = Map.ofEntries(
+                Map.entry(Category.JAPANESE, Menus.of(initJapanese())),
+                Map.entry(Category.KOREAN, Menus.of(initKorean())),
+                Map.entry(Category.CHINESE, Menus.of(initChinese())),
+                Map.entry(Category.ASIAN, Menus.of(initAsian())),
+                Map.entry(Category.WESTERN, Menus.of(initWestern()))
+        );
+
+        return new MenuBoard(initMenus);
+    }
+
+    private List<String> initJapanese() {
+        return List.of("규동", "우동", "미소시루", "스시", "가츠동", "오니기리", "하이라이스", "라멘", "오코노미야끼");
+    }
+
+    private List<String> initKorean() {
+        return List.of("김밥", "김치찌개", "쌈밥", "된장찌개", "비빔밥", "칼국수", "불고기", "떡볶이", "제육볶음");
+    }
+
+    private List<String> initChinese() {
+        return List.of("깐풍기", "볶음면", "동파육", "짜장면", "짬뽕", "마파두부", "탕수육", "토마토", "달걀볶음", "고추잡채");
+    }
+
+    private List<String> initAsian() {
+        return List.of("팟타이", "카오 팟", "나시고렝", "파인애플 볶음밥", "쌀국수", "똠얌꿍", "반미", "월남쌈", "분짜");
+    }
+
+    private List<String> initWestern() {
+        return List.of("라자냐", "그라탱", "뇨끼", "끼슈", "프렌치 토스트", "바게트", "스파게티", "피자", "파니니");
     }
 }
