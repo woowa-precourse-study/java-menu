9abe94549daf1bd7832e483dacd8ea6a2b0b2349
Tae Kyoun Kim <xorbskim6105@gmail.com>
2025-12-21 14:32:51 +0900

    feat : inputview 추가

---
+++ src/main/java/menu/view/InputView.java
@@ -0,0 +1,19 @@
+package menu.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public String[] readCoachNames(){
+        System.out.println("코치의 이름을 입력해 주세요. (, 로 구분)");
+        String inputNames = Console.readLine();
+        String[] names = inputNames.split(",");
+        return names;
+    }
+
+    public String[] readRejectedMenu(String name){
+        System.out.println(name + "(이)가 못 먹는 메뉴를 입력해 주세요.");
+        String inputMenu = Console.readLine();
+        String[] names = inputMenu.split(",");
+        return names;
+    }
+}
